d44a5bdc8e4202a0758b5f4f34b7f1fcc9a190f6
Nar Cuenca <narc.ph@gmail.com>
2019-03-16 18:38:04 +0800

    updated the app to display facts dynamically

--- app/src/main/java/com/narcuenca/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/narcuenca/funfacts/FunFactsActivity.java
@@ -1,13 +1,43 @@
 package com.narcuenca.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+    // Declare our View variables
+    private TextView factTextView;
+    private Button showFactButton;
+    private RelativeLayout mainContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Assign the Views from the layout file to the corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        mainContainer = findViewById(R.id.mainContainer);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = factBook.getFact();
+                int color = colorWheel.getColor();
+                // Update screen with the new fact
+                factTextView.setText(fact);
+                mainContainer.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+
+        showFactButton.setOnClickListener(listener);
     }
 }
